f35757b8b33fdf3b172027aa20f17c64226d8b82
kami <hiroki.zenigami@gmail.com>
2015-07-19 02:38:02 +0900

    Update

--- 1.ex
+++ 1.ex
@@ -1,3 +1,12 @@
 add = fn a, b -> a + b end
 
 IO.puts add.(1, 2) #=> 3
+
+list = [1, 2, 3] ++ [4, 5, 6]
+
+IO.puts Enum.join(list, ",") #=> 1,2,3,4,5,6
+
+IO.puts "hello" <> "world" #=> helloworld
+
+IO.puts true and true      #=> true
+IO.puts true and not false #=> true
