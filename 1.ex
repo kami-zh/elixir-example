bd4bf416a6cddb8b4e2ca8fc7ffe049d917c097f
kami <hiroki.zenigami@gmail.com>
2015-07-19 23:35:55 +0900

    Update

--- 1.ex
+++ 1.ex
@@ -37,4 +37,62 @@ IO.puts ^x = 1 #=> 1
 [a | _] = [1, 2, 3]
 
 IO.puts a #=> 1
-IO.puts _ #=> CompileError
+# IO.puts _ #=> CompileError
+
+case {1, 2, 3} do
+  {4, 5, 6} ->
+    "This clause won't match"
+  {1, x, 3} ->
+    "This clause will match and bind x to 2 in this clause"
+  _ ->
+    "This clause would match any value"
+end
+
+case 10 do
+  ^x -> "Won't match"
+  _  -> "Will match"
+end
+
+case {1, 2, 3} do
+  {1, x, 3} when x > 0 ->
+    "Will match"
+  _ ->
+    "Would match, if guard condition were not satisfied"
+end
+
+f = fn
+  x, y when x > 0 -> x + y
+  x, y -> x * y
+end
+
+f.(2, 3)  #=> 5
+f.(-2, 3) #=> -6
+
+cond do
+  1 + 2 == 4 ->
+    "This is never true"
+  2 + 2 == 4 ->
+    "But this will"
+end
+
+if true do
+  "This works!"
+end
+
+unless true do
+  "This will never be seen"
+end
+
+if true, do: 1 + 2 #=> 3
+
+defmodule Math do
+  def sum(x) do
+    if x == 0 do
+      0
+    else
+      x + sum(x - 1)
+    end
+  end
+end
+
+Math.sum(10) #=> 55
